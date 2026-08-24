844556b3a9512ab6ab5fb3e45b04a7c6820ea2b7
dabid chu <arranzd5@gmail.com>
2026-08-24 10:51:20 +0200

    feat(products): add navigation from cardas to product detail

--- src/app/components/producto-card/producto-card.ts
+++ src/app/components/producto-card/producto-card.ts
@@ -1,11 +1,12 @@
 import { Component, input, inject } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { Producto } from '../../models/producto';
 import { CarritoService } from '../../services/carrito';
 
 @Component({
   selector: 'app-producto-card',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, RouterLink],
   templateUrl: './producto-card.html',
   styleUrl: './producto-card.scss',
 })
